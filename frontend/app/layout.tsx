da1489903c9a5d035f94ab42d3f100334250dc13
Satyam Namdev <namdev2003satyam@gmail.com>
2024-10-25 02:00:02 +0530

    Added vercel analytics

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -17,7 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="dark">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
